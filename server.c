10e3a47e65f4d613c8ec315272582579d06a3185
Kapetanakis Giannis <bilias@edu.physics.uoc.gr>
2024-06-21 16:31:23 +0300

    missed this one

--- server.c
+++ server.c
@@ -3612,6 +3612,10 @@ service_remaining_tcp(struct nsd* nsd)
 			if((event&EV_READ))
 				fn = handle_tls_reading;
 			else	fn = handle_tls_writing;
+		else if(p->tls_auth) {
+			if((event&EV_READ))
+				fn = handle_tls_reading;
+			else	fn = handle_tls_writing;
 		} else {
 #endif
 			if((event&EV_READ))
