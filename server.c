cdb30b6dc0456a5d4b3dbac5793b66c32633b70e
Kapetanakis Giannis <bilias@edu.physics.uoc.gr>
2024-06-21 17:42:05 +0300

    new connection, empty both just in case

--- server.c
+++ server.c
@@ -5353,6 +5353,7 @@ handle_tcp_accept(int fd, short event, void* arg)
 
 #ifdef HAVE_SSL
 	if (data->tls_accept) {
+		tcp_data->tls = NULL;
 		tcp_data->tls_auth = NULL;
 		tcp_data->tls = incoming_ssl_fd(tcp_data->nsd->tls_ctx, s);
 		if(!tcp_data->tls) {
@@ -5366,6 +5367,7 @@ handle_tcp_accept(int fd, short event, void* arg)
 			  handle_tls_reading, tcp_data);
 	} else if (data->tls_auth_accept) {
 		tcp_data->tls = NULL;
+		tcp_data->tls_auth = NULL;
 		tcp_data->tls_auth = incoming_ssl_fd(tcp_data->nsd->tls_auth_ctx, s);
 		if(!tcp_data->tls_auth) {
 			close(s);
